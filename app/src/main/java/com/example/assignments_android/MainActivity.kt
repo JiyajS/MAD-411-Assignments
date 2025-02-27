28bb268815e936c1b1bce05efa6df6c9d49d95f2
Jiya Shah <w0835521@myscc.cs>
2025-02-27 00:50:01 -0500

    assignment done

--- app/src/main/java/com/example/assignments_android/MainActivity.kt
+++ app/src/main/java/com/example/assignments_android/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.assignments_android
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var name: EditText
+    private lateinit var text: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        name = findViewById(R.id.editText)
+        text = findViewById(R.id.textViewName)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun showNameClick(view: View) {
+        val name1 = name.text
+        text.setText("Hello "+name1)
+
     }
 }
