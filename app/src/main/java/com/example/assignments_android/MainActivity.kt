392d7c8bedeaad81a075af24aaf1bbdf3212f48c
Jiya Shah <w0835521@myscc.cs>
2025-03-12 18:30:01 -0400

    half way to complete assignments

--- app/src/main/java/com/example/assignments_android/MainActivity.kt
+++ app/src/main/java/com/example/assignments_android/MainActivity.kt
@@ -2,32 +2,29 @@ package com.example.assignments_android
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.assignments_android.R.id
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var name: EditText
-    private lateinit var text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        name = findViewById(R.id.editText)
-        text = findViewById(R.id.textViewName)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val expenseInput = findViewById<EditText>(R.id.editText)
+        val amount = findViewById<EditText>(R.id.amountInput)
+        val button = findViewById<Button>(R.id.addExpense)
+
+        button.setOnClickListener(){
+
         }
     }
 
-    fun showNameClick(view: View) {
-        val name1 = name.text
-        text.setText("Hello "+name1)
 
-    }
 }
