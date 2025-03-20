66edd350f148b5cbc0c4df7faa48630a4b63a23a
Jiya Shah <w0835521@myscc.cs>
2025-03-20 11:38:39 -0400

    assignment8 completed

--- app/src/main/java/com/example/assignments_android/ExpenseDetailsActivity.kt
+++ app/src/main/java/com/example/assignments_android/ExpenseDetailsActivity.kt
@@ -1,4 +1,14 @@
 package com.example.assignments_android
 
-class ExpenseDetailsActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ExpenseDetailsActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.show_details)
+    }
 }
