8161d84cf0d85d8d36dae5e5c44bad1d54db8cac
Jiya Shah <w0835521@myscc.cs>
2025-03-29 22:05:57 -0400

    Read and writing of the file is done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,11 +37,14 @@ android {
 
 dependencies {
 
+    implementation("com.google.code.gson:gson:2.10.1")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/assignments_android/MainActivity.kt
+++ app/src/main/java/com/example/assignments_android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.assignments_android
 
 import ExpenseAdapter
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -13,17 +14,23 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.assignments_android.R.id
-
-
-
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+private const val FILE_NAME = "expenses.txt"
+private val expenseList = mutableListOf<expenses>()
 
 class MainActivity : AppCompatActivity() {
 
+    private val expenseList = mutableListOf<expenses>()
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +56,8 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = ExpenseAdapter(expenseList)
         recyclerView.adapter = adapter
+        expenseList.addAll(loadExpense(this))
+        adapter.notifyDataSetChanged()
 
         tipsButton.setOnClickListener(){
             val url = "https://www.bp.com/en/global/corporate/news-and-insights/press-releases/growing-shareholder-value-a-reset-bp.html#14"
@@ -67,7 +76,10 @@ class MainActivity : AppCompatActivity() {
                 amount.text.clear()
             }
 //            totalExpense(footerFragment, expenseList)
+
+            saveEntryToFile(this, expenseList)
         }
+
 
 
     }
@@ -77,7 +89,40 @@ class MainActivity : AppCompatActivity() {
 //            it.amount?.toDoubleOrNull() ?: 0.0
 //        }
 //        footerFragment.totalExpense(total)
+
 //    }
+    private fun saveEntryToFile(context: Context, expenses: List<expenses>) {
+        try {
+            val json = Gson().toJson(expenses)
+            context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE).use { output ->
+                output.write(json.toByteArray())
+            }
+            Log.d("FileStorage", "Expenses saved successfully")
+        } catch (e: IOException) {
+            Log.e("FileStorage", "Error saving expenses: ${e.message}")
+        }
+
+
+    }
+    private fun loadExpense(context: Context): MutableList<expenses>{
+
+        val expenseList: MutableList<expenses> = mutableListOf()
+        try {
+            val file = File(context.filesDir, FILE_NAME)
+            if (!file.exists()) return expenseList
+
+            val json = file.readText()
+            val type = object : TypeToken<List<expenses>>() {}.type
+            val loadExpenses : List<expenses> = Gson().fromJson(json, type)
+            expenseList.addAll(loadExpenses)
+            Log.d("FileStorage", "Expenses loaded successfully")
+        }catch (e : FileNotFoundException){
+            Log.e("FileStorage", "File not Found: ${e.message}")
+        }catch (e : IOException){
+            Log.e("FileStorage", "Error reading file: ${e.message}")
+        }
+        return expenseList
+    }
         override fun onStart() {
             super.onStart()
             Log.d("ActivityLifecycle", "onStart called")
